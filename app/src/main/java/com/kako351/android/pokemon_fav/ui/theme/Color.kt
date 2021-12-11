e5b486f8a9f4c3678cc8a647528f62cfeb75f4f4
kako351 <h3.birth@gmail.com>
2021-12-12 01:48:20 +0900

    add colors

--- app/src/main/java/com/kako351/android/pokemon_fav/ui/theme/Color.kt
+++ app/src/main/java/com/kako351/android/pokemon_fav/ui/theme/Color.kt
@@ -2,7 +2,13 @@ package com.kako351.android.pokemon_fav.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val PikachuYellow = Color(0xFFf6e652)
+val EeveeCamel = Color(0xFFa4624a)
+val HitokageRed = Color(0xFFff9441)
+val ZenigameBlue = Color(0xFF5a9ca4)
+val FushigidaneGreen = Color(0xFF399494)
+val GroudonRed = Color(0xFFff2029)
+val KyogreBlue = Color(0xFF3962b4)
+val RayquazaGreen = Color(0xFF4a8373)
+val MewWhite = Color(0xFFFFFFFF)
+val BlackyBlack = Color(0xFF2e2f31)
--- app/src/main/java/com/kako351/android/pokemon_fav/ui/theme/Theme.kt
+++ app/src/main/java/com/kako351/android/pokemon_fav/ui/theme/Theme.kt
@@ -1,21 +1,32 @@
 package com.kako351.android.pokemon_fav.ui.theme
 
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonColors
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = PikachuYellow,
+    primaryVariant = EeveeCamel,
+    secondary = HitokageRed,
+    background = BlackyBlack
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = HitokageRed,
+    primaryVariant = GroudonRed,
+    secondary = KyogreBlue,
+    background = MewWhite
 
     /* Other default colors to override
     background = Color.White,
@@ -41,4 +52,31 @@ fun PokemonfavTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Compos
         shapes = Shapes,
         content = content
     )
+}
+
+@Preview(uiMode = UI_MODE_NIGHT_NO)
+@Composable
+fun LightTheme(){
+    PreviewSample()
+}
+
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun DarkTheme(){
+    PreviewSample()
+}
+
+@Composable
+fun PreviewSample() {
+    PokemonfavTheme {
+        Surface {
+            Column {
+                Text(text = "ピカチュウ")
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Primary ボタン")
+                }
+                Text(color = colors.secondary, text = "secondary text")
+            }
+        }
+    }
 }
