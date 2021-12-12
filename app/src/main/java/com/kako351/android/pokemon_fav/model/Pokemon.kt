055332f978c6791c96138f207f36b7edeca8170e
kako351 <h3.birth@gmail.com>
2021-12-12 14:49:49 +0900

    nullable

--- app/src/main/java/com/kako351/android/pokemon_fav/model/Pokemon.kt
+++ app/src/main/java/com/kako351/android/pokemon_fav/model/Pokemon.kt
@@ -4,35 +4,35 @@ import com.squareup.moshi.Json
 
 data class Pokemon(
     @Json(name = "attacks")
-    val attacks: Attacks,
+    val attacks: Attacks? = null,
     @Json(name = "classification")
-    val classification: String,
+    val classification: String? = null,
     @Json(name = "evolutionRequirements")
-    val evolutionRequirements: EvolutionRequirements,
+    val evolutionRequirements: EvolutionRequirements? = null,
     @Json(name = "evolutions")
-    val evolutions: List<Evolution>,
+    val evolutions: List<Evolution>? = null,
     @Json(name = "fleeRate")
-    val fleeRate: Double,
+    val fleeRate: Double? = null,
     @Json(name = "height")
-    val height: Height,
+    val height: Height? = null,
     @Json(name = "id")
     val id: String,
     @Json(name = "image")
-    val image: String,
+    val image: String? = null,
     @Json(name = "maxCP")
-    val maxCP: Int,
+    val maxCP: Int? = null,
     @Json(name = "maxHP")
-    val maxHP: Int,
+    val maxHP: Int? = null,
     @Json(name = "name")
     val name: String,
     @Json(name = "number")
-    val number: String,
+    val number: String? = null,
     @Json(name = "resistant")
-    val resistant: List<String>,
+    val resistant: List<String>? = null,
     @Json(name = "types")
-    val types: List<String>,
+    val types: List<String>? = null,
     @Json(name = "weaknesses")
-    val weaknesses: List<String>,
+    val weaknesses: List<String>? = null,
     @Json(name = "weight")
-    val weight: Weight
+    val weight: Weight? = null
 )
