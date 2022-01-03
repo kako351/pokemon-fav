922483270536fe1dcfeb23623140bf8826245721
kako351 <h3.birth@gmail.com>
2022-01-03 22:02:57 +0900

    add viewModel()

--- app/src/main/java/com/kako351/android/pokemon_fav/screen/HomeScreen.kt
+++ app/src/main/java/com/kako351/android/pokemon_fav/screen/HomeScreen.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.kako351.android.pokemon_fav.R
 import com.kako351.android.pokemon_fav.components.CardColumnCell
